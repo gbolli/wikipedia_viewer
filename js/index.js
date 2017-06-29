5419bc02f2fa0c71c473cc3a826eca4bc8f0f806
Gord Bolli <gord@bolli.ca>
2017-06-29 08:39:19 -0700

    Added working links to random list

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   
   var searchWord = "";
   var apiUrlBase = "https://en.wikipedia.org/w/api.php?";
+  var corsUrl = "https://cors-anywhere.herokuapp.com/"  // "https://crossorigin.me/"
   var results = [];
 
   // Click on search button
@@ -24,7 +25,7 @@ $(document).ready(function() {
   // API call function
 
   function callWiki() {
-    var apiUrl = "http://crossorigin.me/" + apiUrlBase;
+    var apiUrl = corsUrl + apiUrlBase;
     var searchText = $(".searchText").val();
     
     $.ajax({
@@ -51,10 +52,13 @@ $(document).ready(function() {
     });
   }
   
-  // Random Wiki call function
+  // Random Wiki call function (for a list)
+
+  // or use https://en.wikipedia.org/wiki/Special:Random
 
   function callWikiRandom() {
-    var apiUrl = "http://crossorigin.me/" + apiUrlBase;
+    var apiUrl = corsUrl + apiUrlBase;
+    var wikiUrlBase = "https://en.wikipedia.org/wiki/";
     
     $.ajax({
       type: 'GET',
@@ -64,18 +68,21 @@ $(document).ready(function() {
         "action": "query",
         "format": "json",
         "list": "random",
+        "rnnamespace": 0,
         "rnlimit": 5
       },
       
       success: function(data) {
         
         console.log(data); // testing
-        
+
         for (var i = 0; i < 5; i++) {
-          $("#btn"+i+" span").html(data.query.random[i].title);
-          //$("#btn"+i+" p").html(data[2][i]);
-          //$("#btn"+i).attr("href", data[3][i]);
-          $("#btn"+i).removeClass('hidden');
+          var wikiRandomName = data.query.random[i].title;
+          $("#btn"+i+" span").html(wikiRandomName);
+          $("#btn"+i).removeClass('hidden');          
+          // $("#btn"+i+" p").html(data2[2][i]);   // No paragraph for random
+          $("#btn"+i).attr("href", wikiUrlBase + wikiRandomName);
+          
         };
       }
     });
