57cf6ed312d050e6dec2ac760aefe060cd6e7c3d
Gord Bolli <gord@bolli.ca>
2017-06-28 17:05:02 -0700

    Added button content, search button and API call

--- js/index.js
+++ js/index.js
@@ -9,23 +9,37 @@ $(document).ready(function() {
   // URL items
   // "?action=query&list=search&srsearch="+$(".searchText").val()+"&format=json&callback=?"
   
-  // Capture search box
+  // Click on search button
+
+  $(".searchSubmit").on("click", function() {
+    callWiki();
+  });
   
   
   // API call function
   function callWiki() {
-    var apiUrl = apiUrlBase + ;
+    var apiUrl = "http://crossorigin.me/" + apiUrlBase;
+    var searchText = $(".searchText").val();
     
     $.ajax({
       type: 'GET',
       dataType: 'json',
       url: apiUrl,
+      data: {
+        "action": "opensearch",
+        "format": "json",
+        "search": searchText
+      },
       
       success: function(data) {
         
         console.log(data); // testing
         
-        
+        for (var i = 0; i < 10; i++) {
+          $("#btn"+i+" span").html(data[1][i]);
+          $("#btn"+i+" p").html(data[2][i]);
+          $("#btn"+i).removeClass('hidden');
+        };
         // removeClass('hidden');
         
       }
@@ -35,10 +49,8 @@ $(document).ready(function() {
   // Random Wiki call function
   
   
-  // 
-  
-  
-  
+  // Testing
+  // callWiki();
   
   
 });
